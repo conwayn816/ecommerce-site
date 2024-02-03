0fc1a2d8eac8b5166659d1c502cdaeb3d9ed64fa
conwayn816 <77367414+conwayn816@users.noreply.github.com>
2024-02-03 13:45:09 -0500

    registering user works
    Registering a user checks if user already exists. If it doesn't it creates the user with a hashed password and generates a token. Token generation moved to utils folder.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
-import jwt from "jsonwebtoken";
+import generateToken from "../utils/generateToken.js";
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
@@ -11,18 +11,7 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    // create a token
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "30d",
-    });
-
-    //set JWT as HTTP-only cookie
-    res.cookie("jwt", token, {
-      httpOnly: true,
-      sameSite: "strict",
-      secure: process.env.NODE_ENV !== "development",
-      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-    });
+    generateToken(res, user._id);
 
     res.json({
       _id: user._id,
@@ -40,22 +29,49 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/logout
 // @access  Private
 const logoutUser = asyncHandler(async (req, res) => {
-    //clear cookie
-    res.cookie("jwt", "", {
-      httpOnly: true,
-      expires: new Date(0),
-      sameSite: "strict",
-      secure: process.env.NODE_ENV !== "development",
-    });
+  //clear cookie
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+    sameSite: "strict",
+    secure: process.env.NODE_ENV !== "development",
+  });
 
-    res.status(200).json({ message: "Logged out successfully" });
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 // @desc    register user
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.send("register user");
+  const { name, email, password } = req.body;
+
+  const userExits = await User.findOne({ email });
+
+  if (userExits) {
+    res.status(400);
+    throw new Error("User already exists");
+  } else {
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    if (user) {
+      generateToken(res, user._id);
+
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid user data");
+    }
+  }
 });
 
 // @desc    get user profile
