8d2ad43e4ec7b4acbd6cf4a31fd8e985e7ab5d36
conwayn816 <77367414+conwayn816@users.noreply.github.com>
2024-02-12 13:37:20 -0500

    added get orders controllers
    Can get all orders for logged in user or by id

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -41,14 +41,24 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @route   GET /api/orders/myorders
 // @access  Private
 const getMyOrders = asyncHandler(async (req, res) => {
-  res.send("get my orders");
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
 });
 
 // @desc    Get order by ID
 // @route   GET /api/orders/:id
 // @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
-  res.send("get order by id");
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 // @desc    Update order to paid
