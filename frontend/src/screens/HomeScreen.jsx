fef71320e43a86b3754e35c278c2662937c1eb86
conwayn816 <77367414+conwayn816@users.noreply.github.com>
2024-02-26 11:32:35 -0500

    search box added to header

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,5 +1,5 @@
 import { Row, Col } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Product from "../components/Product";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
@@ -15,7 +15,11 @@ const HomeScreen = () => {
 
   return (
     <>
-      <h1>Latest Products</h1>
+      {keyword && (
+        <Link to="/" className="btn btn-light mb-4">
+          Go Back
+        </Link>
+      )}
       {isLoading ? (
         <Loader />
       ) : error ? (
@@ -24,6 +28,7 @@ const HomeScreen = () => {
         </Message>
       ) : (
         <>
+          <h1>Latest Products</h1>
           <Row>
             {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
